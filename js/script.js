34ed2c4a5d3f5399c51c9db02815d6526b9feda0
Milena Wasilewska <milena.wasilewska1998@gmail.com>
2023-01-16 20:33:04 +0100

    functionAdded

--- js/script.js
+++ js/script.js
@@ -1,16 +1,30 @@
-console.log("Cześć!");
+{
+    const welcome = () => {
+        console.log("Cześć!");
+    };
 
-let button = document.querySelector(".js-sectionButton");
-let textEducation = document.querySelector(".js-section__paragraph");
+    const toggleDisplayTextEducationClick = () => {
 
-button.addEventListener("click", () => {
-    textEducation.classList.toggle("show");
+        const textEducation = document.querySelector(".js-section__paragraph");
 
-    if (textEducation.classList.contains("show")) {
-        textEducation.innerText = "Zdobyłam tytuł inżyniera magistra na kierunku Mechaniki i Budowy Maszyn na uczelni AGH Kraków.";
-        button.innerText = "Ukryj informacje o wykształceniu";
-    } else {
-        textEducation.innerText = "";
-        button.innerText = "Wyświetl informacje o wykształceniu";
+        textEducation.classList.toggle("show");
+
+        if (textEducation.classList.contains("show")) {
+            textEducation.innerText = "Zdobyłam tytuł inżyniera magistra na kierunku Mechaniki i Budowy Maszyn na uczelni AGH Kraków.";
+            button.innerText = "Ukryj informacje o wykształceniu";
+        } else {
+            textEducation.innerText = "";
+            button.innerText = "Wyświetl informacje o wykształceniu";
+        }
     }
-});
+
+    const init = () => {
+
+        const button = document.querySelector(".js-sectionButton");
+        button.addEventListener("click", toggleDisplayTextEducationClick);
+
+        welcome();
+    }
+
+    init();
+}
